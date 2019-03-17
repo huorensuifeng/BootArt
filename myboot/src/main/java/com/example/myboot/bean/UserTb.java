deff8f5671c8811e6b2736e904496430ca4f1c19
zhujun <zhujun_89@163.com>
2019-03-17 21:25:33 +0800

    myroot 数据库user

---
+++ myboot/src/main/java/com/example/myboot/bean/UserTb.java
@@ -0,0 +1,41 @@
+package com.example.myboot.bean;
+
+public class UserTb {
+
+    private String name;
+    private String sex;
+    private int id;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
